3785b810382725bc05399d26d27d96a37a51aff5
Mani@123 <KADAMBAMANIKANTA311@GMAIL.COM>
2024-05-28 19:43:31 +0530

    setup.py added

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r",encoding =" utf-8" ) as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "songs-recommendation-using-facial-emotions"
+AUTHOR_NAME = "kadManikanta"
+SRC_REPO = "songs-recommendation-using-facial-emotions"
+AUTHOR_EMAIL = "kadambamanikanta311@gmail.com"
+
+
+setuptools.setup(
+    name= SRC_REPO,
+    version=__version__,
+    author=AUTHOR_NAME,
+    author_email= AUTHOR_EMAIL,
+    description=" ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_NAME}/{REPO_NAME}/issues",
+    },
+    license="MIT License",
+    package_dir= {"":"src"},
+    packages=setuptools.find_packages(where =" src ")
+)
